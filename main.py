4f3f931506dd4331b63a02bfae5bae8ec345c618
Daria Novikova <khorwl@yandex.ru>
2019-03-09 00:45:30 +0500

    прописала документацию

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ from writer import Writer
 
 
 def get_output_filename():
-    parser = argparse.ArgumentParser(description='jkljkl'
-                                                 'njknjknj'
-                                                 'nkljkl')
+    parser = argparse.ArgumentParser(description='Sniffer catches network packets passing through the personal '
+                                                 'computer, the data is stored in the file specified when the program '
+                                                 'starts in pcap format (the file can be opened in Wireshark). Also '
+                                                 'parsed package headers are displayed in the console. '
+                                                 'This program require superuser privileges.')
+
     parser.add_argument('-o', '--out', help='output pcap file')
     args = parser.parse_args()
 
